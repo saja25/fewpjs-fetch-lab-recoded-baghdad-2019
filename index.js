f6e86e0badccdd54f5b3ffea5b6d7b3144f6481f
saja amer <sajaamer86@gmail.com>
2019-09-26 17:02:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function fetchBooks() {
   fetch('https://anapioficeandfire.com/api/books')
   .then((json)=>response.json())
-  .then(renderBooks)=>json()
+  .then(renderBooks)
 }
 
 function renderBooks(json) {
